de82c7d1ee07a3113039522ca06649c3a83a8d27
ogokdas <ogokdas@yandex.com>
2022-02-11 20:33:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calisanlar C1 = new calisanlar();
+            C1.isim = "Ömer";
+            C1.soyad = "Gökdaş";
+
+            C1.yazdir();
+            Console.ReadLine();
+
+
+    }
+    }
+
+    public class calisanlar
+    {
+        public string isim { get; set; }
+        public string soyad { get; set; }
+
+        public void yazdir()
+        {
+            Console.WriteLine("Çalışan Kişinin Adı: {0}, Soyadı: {1}",isim,soyad);
+
+        }
+    }
+}
